29b86ac13f08894035d0a285b098e9c5fe565496
aymankillua <aymannon95@gmail.com>
2019-11-11 12:33:23 +0200

    added nodeed

---
+++ build/precache-manifest.880f243d638a41ff1b1d49bcd66d011f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced5b5d8bdefe9df1549820b1d002eb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c0ff26af1ee3b82f896",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2b585e793bd69bf7b490",
+    "url": "/static/js/2.0f9e2f77.chunk.js"
+  },
+  {
+    "revision": "9c0ff26af1ee3b82f896",
+    "url": "/static/js/main.501130b6.chunk.js"
+  },
+  {
+    "revision": "f784410b3848e6131fcd",
+    "url": "/static/js/runtime-main.019e21cb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
